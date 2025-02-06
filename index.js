334f00f89999dad61a05e334b15d10695640e273
unknown <MM548@ccs.local>
2025-02-06 18:09:06 +0800

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 5000;
 //Serve static files from "public folder"
 app.use(express.static('public'));
 
+app.use (express.json()); //Middleware to parse JSON bodies
+
 //Define a route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -15,16 +17,32 @@ app.get('/about', (req, res)=> {
     res.send('About Us');
 });
 
-app.use (express.json()); //Middleware to parse JSON bodies
+
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 app.post('/submit', (req, res)=>{
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 })
 
-app.use((req, res, next)=>{
-    console.log(`${req.method} ${req.url}`);
-    next();
+app.use((err, req, res, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const items = ['Apple', 'Banana','Orange'];
+
+app.get('/items', (req, res) =>{
+    res.json(items);
+});
+
+app.post('/items', (req, res) =>{
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
 });
 
 //Start the server
